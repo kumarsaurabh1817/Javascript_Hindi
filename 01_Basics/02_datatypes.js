5b7ec3bdcd3a7c08686952cc7339db7788d54207
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-01-30 09:26:03 +0000

    datatyeps

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treal all the JS code as the newer version of JS
+
+// this alert will give error in node js , it was meant to use in the browser console window
+// alert(3+ )
+
+// after applying semicolon it would work but it is not readable, as in industry you always write codes which are readable and clearly understandable
+// console.log(3+3); console.log("Saurabh") 
+
+
+let name="saurabh" // string data type
+let age=18 // numerical data type
+let isloggedin=false // boolean datatyep
+let state
+
+
+// number - 2 to power 53
+// bigint - for big integers
+// string - can be used in single as well double quoutes but always do in double qoutoes
+// boolean - simply true or false
+// null - standalone value in JS
+// undefineed - value of let is not defined
+// symbol - uniques
+
+// object - 
+// console.log(typeof "saurabh")
+
+console.log(typeof null) // output is object
+console.log(typeof undefined) // output is datatpye
