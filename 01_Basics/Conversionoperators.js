a3212694f63798d4d31969ce7ee4f277c65891e6
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-01-31 17:00:53 +0000

    convertions and operators

---
+++ 01_Basics/Conversionoperators.js
@@ -0,0 +1,91 @@
+/*
+
+let score="33abc"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueinnumber=Number(score)
+console.log(valueinnumber)
+console.log(typeof valueinnumber)
+console.log(typeof (valueinnumber))
+
+*/
+
+// let score=null
+// let score=undefined
+// let score=false
+// let score="saurabh"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueinnumber=Number(score)
+// console.log(valueinnumber)
+// console.log(typeof valueinnumber)
+// console.log(typeof (valueinnumber))
+
+/* 
+Basic notes : -
+"33" -> 33
+"33abc" -> NaN
+true - 1, flase - 0
+*/
+
+
+// let isloggedin=1
+// if we convert empty string to a boolean it would give fllse of boolean type
+// let isloggedin=""
+// if we convert Non empty string to a boolean it would give true of boolean type
+// let booleanisloggedin=String(isloggedin)
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(typeof booleanisloggedin)
+// console.log(booleanisloggedin)
+
+
+
+/* Operations in JavaScript  */
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// // double star means 2 raised to power of 3
+// console.log(2**5) 
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+// let gamecounter=100
+// console.log(gamecounter);
+// gamecounter++;
+// console.log(gamecounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
