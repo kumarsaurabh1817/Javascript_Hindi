0b2eb7747d45727b6330e701b1995a85892803f3
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-02-01 17:07:36 +0000

    stringsinjs

---
+++ 01_Basics/strings.js
@@ -0,0 +1,41 @@
+// const name="hitesh"
+// const repoCount=50
+// console.log(name + repoCount + " Value")
+// console.log('Hello my name is ${name} and my repo count is ${repoCount} ');
+
+const gameName=new String('Hitesh-sir-kaise-ho')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.lastIndexOf('r'));
+// console.log(gameName.charAt(8));
+
+
+// const newstring=gameName.substring(0,4);
+// console.log(newstring);
+
+// const anotherstring=gameName.slice(-9,4)
+// console.log(anotherstring);
+
+// const newstringone="    hitesh   "
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+
+const url="https://chat.deepseek.com/a/chat/s/a0ac2e47-70b1-4522-8960-4edd0f180c6a"
+// console.log(url.replace('a','harekrishna'));
+// console.log(url.includes('hare'))
+
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
+
