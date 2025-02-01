443b7f3cc333de389969ad1e28a399de92342f88
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-02-01 13:42:40 +0000

    comparison

---
+++ 01_Basics/Comparison.js
@@ -0,0 +1,33 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+/*
+// console.log(null > 0);
+// In JS , equality check and comparison operators works differenlty that's why they will produce different resutls 
+// console.log(null == 0);
+// console.log(null >= 0);
+these types of comparisons are should be avoided to keep the code clean as much as possible
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+*/
+
+
+
+
+// strict check - this check the values as well as the data type
+console.log("2"==2); // this is equality check , here conversion takes place
+console.log("2"===2); // strict check - this will say both have diff datatype so they are not same hence print false as the output
+
+
+
