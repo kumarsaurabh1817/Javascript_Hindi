dda4f50775c0d1e431b382b3b81be26fd33ed182
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-02-02 08:27:24 +0000

    mathsandnums

---
+++ 01_Basics/number.js
@@ -0,0 +1,100 @@
+// const score=400
+// console.log(score);
+
+// const balance=new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+// const otherNumber=23.8966
+// console.log(otherNumber.toPrecision());
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+
+
+/* maths in js */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(-4.7));
+// console.log(Math.min(4,0,3,6,4,-1,-2,6,9));
+// console.log(Math.max(4,0,3,6,4,-1,-2,6,9));
+
+// console.log(Math.random()*10); //always between 0 and 1
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
