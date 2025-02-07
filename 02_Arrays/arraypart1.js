4fcbb8ef6b41fd4e4bcf1365a88d3251ea021f0b
Saurabh Kumar <134209734+kumarsaurabh1817@users.noreply.github.com>
2025-02-07 12:27:09 +0000

    arraypart1

---
+++ 02_Arrays/arraypart1.js
@@ -0,0 +1,34 @@
+// arrays declration
+const myarr=[0,1,2,3,4,5]
+// const myheroes=["shaktimann","Naagraj"]
+// const newarray=new Array(10,2,0,4,41)
+// all three are correct declaration of the arrays
+// console.log(myarr)
+
+// arrays methods
+// myarr.push(6);
+// myarr.push(7);
+// console.log(myarr)
+// myarr.pop();
+
+
+// myarr.unshift(0)
+// myarr.unshift(9) 
+// unshift will push in the start of array
+// myarr.shift(9) 
+// shift will pop from the start of array
+// console.log(myarr.includes(9)) // check whether the value is present in the array or not, and return true or false respectivelyu
+// console.log(myarr.includes(1))
+
+// indexof gives the index of the element , if does not exists then it returns a -1
+
+// console.log(myarr.indexOf(2222))
+// const newarr= myarr.join() /// this gives string
+// console.log(typeof myarr); // this is array
+// console.log(typeof newarr); // this is string
+
+
+
+
+
+// slice and splice
